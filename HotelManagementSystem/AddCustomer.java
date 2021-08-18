d897788f30d658232c2d0980569e09b45f4e2047
ritik1305 <ritik1305@gmail.com>
2021-08-18 13:08:51 +0530

    add all files made till 18-08-2021

---
+++ HotelManagementSystem/AddCustomer.java
@@ -0,0 +1,26 @@
+package HotelManagementSystem;
+
+import javax.swing.*;
+
+
+public class AddCustomer extends JFrame{
+	
+	
+	AddCustomer(){
+		
+		
+		JLabel l1 = new JLabel("New Customer Form");
+		l1.setBounds(100,120,300,30);
+		add(l1);
+		
+		setLayout(null);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		
+		new 
+
+	}
+
+}
